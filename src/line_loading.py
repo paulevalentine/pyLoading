510c915d8b3e1ad67a3af718f8cb02311c77e8ab
paulevalentine <62242813+paulevalentine@users.noreply.github.com>
2026-05-04 14:30:19 +0100

    Updated path to data.

--- src/line_loading.py
+++ src/line_loading.py
@@ -7,37 +7,37 @@ pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_columns', None)
 
 def calc_line_loads():
-    unit_loads = pd.read_csv('../data/unit_loads.csv', index_col=0)
+    unit_loads = pd.read_csv('./unit_loads.csv', index_col=0)
     print("Unit permanent loads (kN/m^2):")
     unit_loads=unit_loads.iloc[:,:-1]
     unit_loads['Total gk'] = unit_loads.sum(axis=1)
-    unit_loads.to_csv('../data/unit_loads.csv')
+    unit_loads.to_csv('./unit_loads.csv')
     display(unit_loads)
 
-    variable_file = Path('../data/variable_loads.csv')
+    variable_file = Path('./variable_loads.csv')
     if variable_file.exists():
-        variable_loads = pd.read_csv('../data/variable_loads.csv', index_col=0)
+        variable_loads = pd.read_csv('./variable_loads.csv', index_col=0)
         if variable_loads.index.tolist() != unit_loads.index.tolist():
             print("Error : Variable load categories do not match dead load data")
             display(variable_loads.head())
             return 0
     else:
         variable_loads = pd.DataFrame(index=unit_loads.index, columns=['qk'])
-        variable_loads.to_csv('../data/variable_loads.csv')
+        variable_loads.to_csv('./variable_loads.csv')
 
     print("Unit variable loads (kN/m^2):")
     display(variable_loads)
 
-    line_file = Path('../data/load_lines.csv')
+    line_file = Path('./load_lines.csv')
     if line_file.exists():
-        load_lines = pd.read_csv('../data/load_lines.csv', index_col=0)
+        load_lines = pd.read_csv('./load_lines.csv', index_col=0)
         if load_lines.columns.tolist() != unit_loads.index.tolist():
             print("Error : line load load categories do no math span data")
             display(load_lines.head())
             return 0
     else:
         load_lines = pd.DataFrame(columns=unit_loads.index)
-        load_lines.to_csv('../data/load_lines.csv')
+        load_lines.to_csv('./load_lines.csv')
 
     print("Supported lengths (m):")
     display(load_lines)
